ea6a288c568914b2e31278099ae3a093702e9f3c
Gabriel Freitas <freitasgabrield.o.a@gmail.com>
2023-08-08 21:09:50 -0300

    Finalizando verificação de escolaridade

--- js/script.js
+++ js/script.js
@@ -47,16 +47,64 @@ endereco.addEventListener("blur", function(){
 let email = document.querySelector("input[name=email]")
 let confirmar = document.querySelector("input[name=confirmar")
 
-let minusculo = email.value.toLowerCase() // Tornando o conteudo do email minúsculo
-
 email.addEventListener("keyup", function(){
     confirmar.value = email.value
+    let minusculo = email.value.toLowerCase() // Tornando o conteudo do email minúsculo
     
-    if (minusculo.indexOf("@") == -1 ) {
+    if (minusculo.indexOf('@') == -1 || minusculo.indexOf('.com') == -1) {
+        //Executa se for verdade
+        console.log("Email invalido")
+        //email.style.border = "2px solid red"
+        email.classList.add("border-danger")
+        email.classList.remove("border-success")
 
+        confirmar.classList.add("border-danger")
+        confirmar.classList.remove("border-success")
     }
     else {
+        console.log("Email valido")
+        //email.style.border = "2px solid green"
+        email.classList.add("border-success")
+        email.classList.remove("border-danger")
+
+        confirmar.classList.add("border-success")
+        confirmar.classList.remove("border-danger")
 
     }
-    console.log (email.value.indexOf("@"))
+    console.log (email.value.indexOf('@'))
+})
+
+let senha = document.querySelector("#senha")
+let btn_olho = document.querySelector(".fa-eye")
+
+//()=> {} Istó é uma arrow function, ou seja, uma versão resumida da função anônima.
+btn_olho.addEventListener("click",()=>{
+     if(btn_olho.classList.contains("fa-eye")){
+        console.log ("A classe existe")
+        btn_olho.classList.remove("fa-eye")
+        btn_olho.classList.add("fa-eye-slash")
+
+        senha.setAttribute("type", "text")
+     }
+     else{
+        console.log ("A classe não existe")
+        btn_olho.classList.remove("fa-eye-slash")
+        btn_olho.classList.add("fa-eye")
+
+        senha.setAttribute("type", "password")
+
+     }
+})
+
+// PEGANDO DADOS DO CAMPO DE RADIO
+
+let escolaridade = document.querySelectorAll("input[name=escolaridade]")
+let btnEscolaridade = document.querySelector("#btnEscolaridade")
+
+btnEscolaridade.addEventListener("click", (evento)=>{
+    console.log(evento)
+    evento.preventDefault() // Está função ira impedir o comportamento padrão do botão
+
+    console.log (escolaridade)
+
 })
